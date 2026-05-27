0db66cce0cccaf9cd5356bd890d5d9ba418fbfc3
Oleg Shilov <shilov-1@yandex.ru>
2026-05-27 16:38:16 +0300

    up limits;

--- src/middlewares/securityMiddleware.ts
+++ src/middlewares/securityMiddleware.ts
@@ -9,7 +9,7 @@ export const helmetMiddleware = helmet({
 
 export const apiRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 200,
+  limit: 1000,
   standardHeaders: 'draft-8',
   legacyHeaders: false,
   message: {
